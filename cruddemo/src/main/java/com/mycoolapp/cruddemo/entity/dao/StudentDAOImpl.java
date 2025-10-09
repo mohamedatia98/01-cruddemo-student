6f8b9f377c7aee4329476a17013fa06b7bd346f2
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-09 10:47:19 +0300

    creating class implement from interface and define entity manager and constructor and implement save method override .

---
+++ cruddemo/src/main/java/com/mycoolapp/cruddemo/entity/dao/StudentDAOImpl.java
@@ -0,0 +1,33 @@
+package com.mycoolapp.cruddemo.entity.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mycoolapp.cruddemo.entity.Student;
+
+import jakarta.persistence.EntityManager;
+
+// Specialized annotation for repository classes 
+// Supports Components Scanning
+// Translate JDBC Exceptions 
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    // define field Entity Manager
+    private EntityManager entityManager;
+
+    // define constructor
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+    @Override
+    @Transactional
+    public void save(Student student) {
+        entityManager.persist(student);
+    }
+
+}
