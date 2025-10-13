0ef00a8b5026bc95b75a2d08d0662e406911e10e
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-13 14:13:49 +0300

    ADDING ALL CRUD application so far

--- cruddemo/src/main/java/com/mycoolapp/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/mycoolapp/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.mycoolapp.cruddemo;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,15 +20,79 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentdao) {
 		return runner -> {
-			CreateAndReadStudent(studentdao);
+			// CreateAndReadStudent(studentdao);
+
+			// queryForStudents(studentdao);
+
+			// queryStudentsByLastName(studentdao, "ismail");
+
+			// updateStudent(studentdao);
+
+			// deleteStudent(studentdao);
+
+			deleteAllStudents(studentdao);
+
 		};
+	}
+
+	private void deleteAllStudents(StudentDAO studentdao) {
+		// delete all
+		System.out.println("Deleting all Students :");
+		int numsofrows = studentdao.deleteAll();
+
+		System.out.println("Deleted Rows Count : " + numsofrows);
+	}
+
+	private void deleteStudent(StudentDAO studentdao) {
+		// getting id of student
+		int studentid = 1;
+		System.out.println("Deleting student id : " + studentid);
+
+		studentdao.delete(studentid);
+	}
+
+	private void updateStudent(StudentDAO studentdao) {
+		// Retrieve student by specific id
+		int studentId = 3;
+		System.out.println("Getting Student with ID : " + studentId);
+		Student mystudent = studentdao.findById(studentId);
+
+		// change firstName to "John"
+		System.out.println("updating the student !");
+		mystudent.setFirstName("John");
+
+		// update the student
+		studentdao.Update(mystudent);
+
+		// Display the student
+		System.out.println("Updated Student " + mystudent);
+	}
+
+	private void queryStudentsByLastName(StudentDAO studentdao, String string) {
+		List<Student> thestudents = studentdao.findByLastName(string);
+
+		// Display quries
+		for (Student tempstudent : thestudents) {
+			System.out.println(tempstudent);
+		}
+	}
+
+	private void queryForStudents(StudentDAO studentdao) {
+
+		// Generate quries
+		List<Student> thestudents = studentdao.findAll();
+
+		// Display quries
+		for (Student tempstudent : thestudents) {
+			System.out.println(tempstudent);
+		}
 	}
 
 	private void CreateAndReadStudent(StudentDAO studentdao) {
 
 		// create the student object ( Module - Entity - Table Structure )
 		System.out.println("Creating Student Object");
-		Student student = new Student("mohamed", "atia", "hola@gmail.com");
+		Student student = new Student("mohamed", "ismail", "hola99@gmail.com");
 
 		// save the student object
 		System.out.println("Saving Student Object");
