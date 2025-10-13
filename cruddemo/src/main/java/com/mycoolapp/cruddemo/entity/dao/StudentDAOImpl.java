1c6c1ff59f23f3901041f18303ae9f416a804453
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-13 14:14:15 +0300

    adding body of all crud functions

--- cruddemo/src/main/java/com/mycoolapp/cruddemo/entity/dao/StudentDAOImpl.java
+++ cruddemo/src/main/java/com/mycoolapp/cruddemo/entity/dao/StudentDAOImpl.java
@@ -42,10 +42,50 @@ public class StudentDAOImpl implements StudentDAO {
     public List<Student> findAll() {
         // Quries
         // Create query
-        TypedQuery<Student> theQuery = entityManager.createQuery("From Student", Student.class);
+        TypedQuery<Student> theQuery = entityManager.createQuery("From Student order by firstName desc", Student.class);
 
         // Return query Results
         return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        // quries
+        TypedQuery<Student> theQuery = entityManager.createQuery("From Student WHERE lastName=:atia ", Student.class);
+
+        // Set parameter
+        // Because when Hibernate sends this query to the database, it needs actual
+        // values, not placeholders.
+        theQuery.setParameter("atia", theLastName);
+
+        // Return
+        return theQuery.getResultList();
+    }
+
+    @Override
+    @Transactional // perform update into the table , not just queries
+    public void Update(Student student) {
+        entityManager.merge(student);
+    }
+
+    @Override
+    @Transactional // this is a must to run smoothly
+    public void delete(Integer id) {
+        // performing the delete
+        // Retrieve student by id
+        Student muyStudent = entityManager.find(Student.class, id);
+
+        // delete
+        entityManager.remove(muyStudent);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+        // delete all by getting quries on all table .
+        int numofRowsDeleted = entityManager.createQuery("DELETE From Student").executeUpdate();
+
+        return numofRowsDeleted;
     }
 
 }
