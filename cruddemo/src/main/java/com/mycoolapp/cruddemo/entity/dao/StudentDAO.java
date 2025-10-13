aeb3f061bb17247dd7a6a609290d5f6d6c314b02
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-13 14:14:04 +0300

    addding methods on interface

--- cruddemo/src/main/java/com/mycoolapp/cruddemo/entity/dao/StudentDAO.java
+++ cruddemo/src/main/java/com/mycoolapp/cruddemo/entity/dao/StudentDAO.java
@@ -1,5 +1,7 @@
 package com.mycoolapp.cruddemo.entity.dao;
 
+import java.util.List;
+
 import com.mycoolapp.cruddemo.entity.Student;
 
 public interface StudentDAO {
@@ -7,4 +9,15 @@ public interface StudentDAO {
     void save(Student student);
 
     Student findById(long id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String thelastName);
+
+    void Update(Student student);
+
+    void delete(Integer id);
+
+    int deleteAll();
+
 }
